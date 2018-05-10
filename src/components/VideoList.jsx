78ff6e814276d89d7a243c1e767bd88d4a326adb
kiannaquach <kiannaquach.kq@gmail.com>
2018-05-09 17:36:20 -0700

    finished to es6

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,17 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+class VideoList extends React.Component {
+  
+  render() {
+    return (
+      <div className="video-list">
+          {this.props.videos.map((video, i) => 
+            <VideoListEntry videos={video} key={i}/>
+          )}
+      </div>
+    )
+  }
+}
+
+
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
